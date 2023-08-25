9b56dffa90664e6fca874ee7bf3b7020b98d6ab2
Muhammad Sami <sami.siddiq@venturedive.com>
2023-08-25 19:34:26 +0500

    dk

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset', '@babel/preset-env', '@babel/preset-react'],
+  // presets: ['module:metro-react-native-babel-preset', '@babel/preset-env', '@babel/preset-react'],
   // presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
+  // presets: ['module:metro-react-native-babel-preset'],
 };
