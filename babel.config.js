fc46b234dcd695d510ed3e9c9b92514ee35af9ae
Muhammad Sami <sami.siddiq@venturedive.com>
2023-08-28 15:39:00 +0500

    resolve issue which throwing error on project run

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   // presets: ['module:metro-react-native-babel-preset', '@babel/preset-env', '@babel/preset-react'],
   // presets: [['@babel/preset-env', {targets: {node: 'current'}}]],
-  // presets: ['module:metro-react-native-babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: ['@babel/plugin-proposal-export-default-from'],
 };
